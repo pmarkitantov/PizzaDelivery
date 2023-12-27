91d3339bfe6931831b8c21648742108e963c5332
Pavel Markitantov <markitantow@gmail.com>
2023-12-27 23:29:30 +0100

      feat: "Add Firebase Authentication to the project for user sign-in and registration using email and password."

--- PizzaDelivery/Authentication/RegisterOrLoginView.swift
+++ PizzaDelivery/Authentication/RegisterOrLoginView.swift
@@ -5,40 +5,79 @@
 //  Created by Pavel Markitantov on 27/12/2023.
 //
 //
+import FirebaseAuth
 import SwiftUI
 
 struct RegisterOrLoginView: View {
-    @State private var userEmail: String = ""
-    @State private var userPassword: String = ""
+    @State var userEmail: String = ""
+    @State var userPassword: String = ""
     @State var isLoginMode = false
+    @State var isSuccessful = false
 
     var body: some View {
-        ZStack {
-            Color("primaryOrange")
-                .ignoresSafeArea()
-            VStack {
-                Image("mainLogo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300)
-                    .clipShape(Circle())
-                    .padding(.bottom, 100)
-                Text(isLoginMode ? "Already have an account? Log in" : "Enter your email and password for register")
-                    .foregroundColor(Color("textColor"))
-                    .font(.body)
-                    .fontWeight(.semibold)
-                    .padding()
+        NavigationStack {
+            ZStack {
+                Color("primaryOrange")
+                    .ignoresSafeArea()
+                VStack {
+                    Image("mainLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300)
+                        .clipShape(Circle())
+                        .padding(.bottom, 100)
+                    Text(isLoginMode ? "Already have an account? Log in" : "Enter your email and password for register")
+                        .foregroundColor(Color("textColor"))
+                        .font(.body)
+                        .fontWeight(.semibold)
+                        .padding()
+                    Divider()
 
-                TextField("Enter your email", text: $userEmail)
+                    TextField("Enter your email", text: $userEmail)
+                        .padding()
+                        .background(Color("textColor"))
+                        .cornerRadius(40)
+                        .padding()
+                    SecureField("Enter your password", text: $userPassword)
+                        .padding()
+                        .background(Color("textColor"))
+                        .cornerRadius(40)
+                        .padding()
+                    Button("Continue") {
+                        isLoginMode ? loginUser() : registerUser()
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
                     .padding()
-                    .background(Color("textColor"))
+                    .foregroundColor(Color("textColor"))
+                    .background(Color("heavyGreen"))
                     .cornerRadius(40)
                     .padding()
-                SecureField("Enter your password", text: $userPassword)
-                    .padding()
-                    .background(Color("textColor"))
-                    .cornerRadius(40)
-                    .padding()
+                }
+            }
+        }
+        .navigationDestination(isPresented: $isSuccessful) {
+            ContentView()
+        }
+    }
+
+    func registerUser() {
+        Auth.auth().createUser(withEmail: userEmail, password: userPassword) { _, error in
+            if error != nil {
+                print(error?.localizedDescription ?? "Unknown error")
+            } else {
+                print("register succesful!")
+                isSuccessful = true
+            }
+        }
+    }
+
+    func loginUser() {
+        Auth.auth().signIn(withEmail: userEmail, password: userPassword) { _, error in
+            if error != nil {
+                print(error?.localizedDescription ?? "Unknown error")
+            } else {
+                print("login succesful")
+                isSuccessful = true
             }
         }
     }
